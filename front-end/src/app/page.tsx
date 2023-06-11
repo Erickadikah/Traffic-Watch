87d04235f1f85a532e7dfd56f8f58cc954289a44
erickadikah <erickadikah2030@gmail.com>
2023-06-12 00:31:38 +0300

    Nav implementation complete

---
+++ front-end/src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from 'react';
+import Navbar from '../app/components/Navbar';
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar links={[]} />
+    </main>
+  );
+}
+
