00152ba3ed98a97a4d14de7f728cb98520cae577
erickadikah <erickadikah2030@gmail.com>
2023-06-16 03:18:15 +0300

    update feeds ui added && functionality pedding

--- front-end/src/app/components/Map.tsx
+++ front-end/src/app/components/Map.tsx
@@ -1,35 +1,43 @@
-import React from 'react'
-import { useEffect, useRef } from 'react'
-import mapboxgl from 'mapbox-gl'
+import React, { useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
 
 const Map = () => {
   const mapContainerRef = useRef(null);
 
   useEffect(() => {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiamtpdHNhbyIsImEiOiJja2QwMWFvcnYwcDk2MzBsN3p4bXZmbzA1In0.yCKFXODdTQSRykFb60p4jg'
+    mapboxgl.accessToken = 'pk.eyJ1IjoiamtpdHNhbyIsImEiOiJja2QwMWFvcnYwcDk2MzBsN3p4bXZmbzA1In0.yCKFXODdTQSRykFb60p4jg';
+
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: 'mapbox://styles/mapbox/streets-v11',
       zoom: 8,
-      center: [ 36.8219,1.2921]
+      center: [36.8219, 1.2921]
     });
-      //add marker
+
+    // Add marker
     const marker = new mapboxgl.Marker({
-        draggable: true
+      // draggable: true
     })
-        .setLngLat([36.937984,-1.359227])
-        .addTo(map),
-        onDragEnd = () => {
-            const lngLat = marker.getLngLat();
-            console.log('lng: ' + lngLat.lng + '<br />lat: ' + lngLat.lat);
-        }
+      .setLngLat([36.8304, 1.2781])
+      .addTo(map);
+      
 
-    //add navigation control
-  return () => map.remove();
-}, []);
-  
-    return (
-    <div className="mt-50" ref={mapContainerRef}  style={{ width: '100vw', height: '70vh', marginBottom: '90px'}}/>);
-  }
+    const onDragEnd = () => {
+      const lngLat = marker.getLngLat();
+      console.log('lng: ' + lngLat.lng + '<br />lat: ' + lngLat.lat);
+    };
 
-export default Map
+    // Add navigation conrol
+
+    return () => map.remove();
+  }, []);
+
+  return (
+    <div style={{ width: '90vw', height: '70vh', marginRight: '50px'}}>
+      <div ref={mapContainerRef} style={{ height: '100%' }} />
+    </div>
+  );
+};
+
+export default Map;
+
--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -2,13 +2,18 @@
 import React from 'react';
 import Navbar from '../app/components/Navbar';
 import Map from '../app/components/Map';
+import Incidents from '../app/components/Incidents';
 
 export default function Home() {
   return (
-    <main>
+    <main className=' w-full h-screen text-textLight  overflow-x-hidden overflow-scroll border-10'>
       <Navbar links={[]} />
-      <h1 className='text-center'>Map</h1>
+      <div className='h-[88vh] w-full mx-auto p-8 mt-0 border-r-8'>
       <Map />
+      </div>
+      <div className='h-[88vh] w-full mx-auto p-10'>
+      <Incidents />
+      </div>
     </main>
   );
 }
