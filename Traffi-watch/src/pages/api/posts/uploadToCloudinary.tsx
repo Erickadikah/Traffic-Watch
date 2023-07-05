1a9d804c6134d844ef5cd613dbd0e9917adbb1fd
erickadikah <erickadikah2030@gmail.com>
2023-07-06 00:01:43 +0300

    upadates

---
+++ Traffi-watch/src/pages/api/posts/uploadToCloudinary.tsx
@@ -0,0 +1,31 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { connect } from '@/lib/mongo/mongo';
+import Post, { PostType } from '@/lib/mongo/postModel';
+import cloudinary from '@/lib/cloudinary/cloudinary';
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await connect();
+  if (req.method === 'GET') {
+    const data = { message: 'Hello, world!' };
+    res.status(200).json(data);
+  } else {
+// console.log(req.body)
+ const { location, description,image } = JSON.parse(req.body);
+//  console.log(JSON.parse(req.body))
+ const cloudinaryUpload = await cloudinary.uploader.upload(image);
+ const myPost = await Post.create({
+            location,
+            description,
+            image: cloudinaryUpload.secure_url,
+          });
+          await myPost.save();
+          console.log('Created Post:');
+          res.status(200).json({myPost});
+    // res.status(405).json({ message: 'Method Not Allowed' });
+  }
+  if ( req.method === 'DELETE') {
+    const { id } = req.query;
+    const post = await Post.findByIdAndDelete(id);
+    res.status(200).json({ post });
+    cloudinary.uploader.destroy(post.image);
+  }
+}
